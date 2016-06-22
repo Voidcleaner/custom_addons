b762bb8c376ba87778ea818ee2ea02abb8489439
Bao <brady.bao@viasystems.pri>
2016-06-22 10:17:09 +0800

    first version

---
+++ todo_app/__openerp__.py
@@ -0,0 +1,7 @@
+{
+    'name': 'To-Do Application',
+    'description': 'Manage your personal Tasks with this module.',
+    'author': 'Brady Bao',
+    'depends': ['mail'],
+    'application': True,
+}
