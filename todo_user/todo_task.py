ba3a245b03286c8d3d322c500a99d76eb5122ceb
Bao <brady.bao@viasystems.pri>
2016-07-01 14:53:59 +0800

    fix bug in todo_task.py

--- todo_user/todo_task.py
+++ todo_user/todo_task.py
@@ -2,5 +2,5 @@
 from openerp import models, fields ,api
 class TodoTask(models.Model):
     _inherit = 'todo.task'
-    user_id = fields.Many2one('res.user', 'Responsible')
+    user_id = fields.Many2one('res.users', 'Responsible')
     date_deadline = fields.Date('Deadline')
