93691996006ce50a76696fd21f630a58fea448cb
Bao <brady.bao@viasystems.pri>
2016-07-07 14:30:21 +0800

    add many functions

--- todo_user/__openerp__.py
+++ todo_user/__openerp__.py
@@ -3,4 +3,5 @@
     'description': 'Extend the To-Do app to multiuser.',
     'author': 'Brady Bao',
     'depends': ['todo_app'],
+    'data': ['todo_view.xml', 'security/todo_access_rules.xml']
 }
