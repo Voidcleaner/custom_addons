551f9dbda66b581c56ba203b32ab3ff4b9340952
Bao <brady.bao@viasystems.pri>
2016-07-19 10:20:42 +0800

    fix arguments

--- todo_ui/todo_model.py
+++ todo_ui/todo_model.py
@@ -3,12 +3,12 @@ from openerp import models, fields, api
 
 class Tag(models.Model):
     _name = 'todo.task.tag'
-    name = fields.Char('Name', 40, translate=True)
+    name = fields.Char('Name', translate=True)
 
 class Stage(models.Model):
     _name = 'todo.task.stage'
     _order = 'sequence,name'
     _rec_name = 'name'  # default
     _table = 'todo_task_stage'  # default
-    name = fields.Char('Name', 40, translate=True)
+    name = fields.Char('Name', translate=True)
     sequence = fields.Integer('Sequence')
